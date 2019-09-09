7640a2e390590167ae722f2e23101ef0b514ea5d
Daniel Ferrer <danfrrr13@gmail.com>
2019-09-09 14:30:00 -0400

    Refactored to use ContextAPI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+import ProductContext from './contexts/ProductContext.js';
+import CartContext from './contexts/CartContext.js';
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -16,26 +19,18 @@ function App() {
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<CartContext.Provider value={cart}>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<div className="App">
+					<Navigation />
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Route exact path="/cart" component={ShoppingCart} />
+				</div>
+			</ProductContext.Provider>
+		</CartContext.Provider>
 	);
 }
 
